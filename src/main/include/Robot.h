127b31984608bc9f341c701e3943b42ac6ff5e33
No1Suprememe <hungryleo9316@gmail.com>
2019-01-17 16:00:12 -0800

    First Commit

--- src/main/include/Robot.h
+++ src/main/include/Robot.h
@@ -12,14 +12,10 @@
 #include <frc/smartdashboard/SendableChooser.h>
 
 #include "OI.h"
-#include "commands/ExampleCommand.h"
-#include "commands/MyAutoCommand.h"
-#include "subsystems/ExampleSubsystem.h"
 
 class Robot : public frc::TimedRobot {
  public:
-  static ExampleSubsystem m_subsystem;
-  static OI m_oi;
+  static OI oi;
 
   void RobotInit() override;
   void RobotPeriodic() override;
@@ -35,7 +31,5 @@ class Robot : public frc::TimedRobot {
   // Have it null by default so that if testing teleop it
   // doesn't have undefined behavior and potentially crash.
   frc::Command* m_autonomousCommand = nullptr;
-  ExampleCommand m_defaultAuto;
-  MyAutoCommand m_myAuto;
   frc::SendableChooser<frc::Command*> m_chooser;
 };

3a2a98159d5bb5de9564ffb5bffc948cd70e3169
No1Suprememe <hungryleo9316@gmail.com>
2019-01-31 14:39:47 -0800

    global robot added

--- src/main/include/Robot.h
+++ src/main/include/Robot.h
@@ -21,12 +21,12 @@
 
 class Robot : public frc::TimedRobot {
  public:
-  static OI oi;
-  static DriveSystem driveSystem;
-  static ClimbSystem climbSystem;
-  static IntakeSystem intakeSystem;
-  static LiftSystem liftSystem;
-  static WristSystem wristSystem;
+  OI oi;
+  DriveSystem driveSystem;
+  ClimbSystem climbSystem;
+  LiftSystem liftSystem;
+  IntakeSystem intakeSystem;
+  WristSystem wristSystem;
 
   void RobotInit() override;
   void RobotPeriodic() override;
@@ -44,3 +44,5 @@ class Robot : public frc::TimedRobot {
   frc::Command* m_autonomousCommand = nullptr;
   frc::SendableChooser<frc::Command*> m_chooser;
 };
+
+extern Robot globalRobot;

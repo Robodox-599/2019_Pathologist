00d08d3108ed7cf30d97b6fae299a82b5602f298
W34262 <w34262@student.ghchs.com>
2019-01-31 14:53:59 -0800

    Functions to commands

--- src/main/cpp/subsystems/IntakeSystem.cpp
+++ src/main/cpp/subsystems/IntakeSystem.cpp
@@ -11,8 +11,6 @@ IntakeSystem::IntakeSystem() : Subsystem("ExampleSubsystem") {
   HatchPiston = new frc::DoubleSolenoid(1, 2); //dummy values
   HatchPiston2 = new frc::DoubleSolenoid(3, 4);
   Stop = new frc::DigitalInput(1); //dummy values
-  bool OldButton = 0;
-  bool OldButton2 = 0;
 }
 
 void IntakeSystem::InitDefaultCommand() {
@@ -25,64 +23,26 @@ void IntakeSystem::InitDefaultCommand() {
 
 void IntakeSystem::Intake(bool intake)
 {
-    bool CurrentButton = OI.GetRawButton(1);
-
-    if (CurrentButton = OldButton)
-    {
-       if (intake && Stop.Get() == false)
-          {
-           IntakeMotor.Set(ControlMode::PercentOutput, .8);
-           wait (500); //dummy value
-           IntakeMotor.Set(ControlMode::PercentOutput, 0);
-          }
-       else
-          {
-            IntakeMotor.Set(ControlMode::PercentOutput, 0);
-          }
-    }
-    OldButton = CurrentButton;
+     IntakeMotor.Set(ControlMode::PercentOutput, .8);     
 }
 
 void IntakeSystem::Outtake(bool outtake)
 {
-   bool CurrentButton2 = OI.GetRawButton(2);
-   
-   if(CurrentButton 2 = OldButton2)
-   {
-     if (outtake && Stop.Get() == false)
-     {
-       IntakeMotor.Set(ControlMode::PercentOutput, -0.8)
-       wait (500); //dummy value
-       IntakeMotor.Set(ControlMode::PercentOutput, 0);
-     }
-     else
-     {
-       IntakeMotor.Set(ControlMode::PercentOutput, 0)
-     }
-   }
-   OldButton2 = CurrentButton2; 
-}
+     IntakeMotor.Set(ControlMode::PercentOutput, -0.8)
+}    
 
 void IntakeSystem::HatchPistons(bool forward)
 {
-    if (forward)
-     {
        HatchPiston.Set(Forward);
        HatchPiston2.Set(Forward);
        Wait (500); //dummy value
        HatchPiston.Set(off);
        HatchPiston.Set(off);
-       toggle = 0;
-     }
-     else
-     {
-       HatchPiston.Set(off);
-       HatchPiston2.Set(off);
-     }
 }
 
-bool Stop()
+bool IntakeSystem::LimitSwitch()
 {
   return Stop.Get();
 }
+
 

1f4dd1be78a8942d241bbb6f37abcda7783b787f
No1Suprememe <hungryleo9316@gmail.com>
2019-02-12 15:19:49 -0800

    copied master and deleted lift

--- src/main/cpp/subsystems/SlideSystem.cpp
+++ src/main/cpp/subsystems/SlideSystem.cpp
@@ -5,37 +5,14 @@
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
 
-#include "subsystems/LiftSystem.h"
-#include <math>
+#include "subsystems/SlideSystem.h"
 
-LiftSystem::LiftSystem() : Subsystem("ExampleSubsystem"), LiftMotor(3), power = 0, degree = 0 {
-  LiftMotor.ConfigSelectedFeedbackSensor(FeedbackDevice::CTRE_MagEncoder_Absolute);
-}
+SlideSystem::SlideSystem() : Subsystem("SlideSystem") {}
 
-void LiftSystem::InitDefaultCommand() {
- 
+void SlideSystem::InitDefaultCommand() {
   // Set the default command for a subsystem here.
   // SetDefaultCommand(new MySpecialCommand());
 }
- 
- void LiftSystem::Thing(double kfU, double kfD, double target)
- {
-   if(target = negative_int_value)
-   {
-   angle = LiftMotor.GetSelectedSensorPosition();
-   angle = angle * 2048/pi;
-   power = kfU * math.cos(degree) * angle;
-   return;
-   }
-   angle = LiftMotor.GetSelectedSensorPosition();
-   angle = angle * 2048/pi;
-   power = kfD * math.cos(degree) * angle;
- }
-
- //kf cost r
-
-  
-
 
 // Put methods for controlling this subsystem
 // here. Call these from Commands.
